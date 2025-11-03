43aa561195c4f47ba2c541c4b72dea773865dc7d
Toasted <iacobdragos04@gmail.com>
2025-11-03 15:02:14 +0200

    Your descriptive commit message

--- apps/frontend/src/Components/SignUp/SignUp.jsx
+++ apps/frontend/src/Components/SignUp/SignUp.jsx
@@ -1,38 +1,41 @@
 import React from "react";
 import './SignUp.css';
-import Facebook_Logo from '../Assets/Facebook_Logo.png';
+import Facebook_Logo from '../Assets/Facebook_Logo.svg';
 import LogoAlb from '../Assets/LogoAlb.svg';
-import Google_Logo from '../Assets/Google_Logo.png';
+import Google_Logo from '../Assets/Google_Logo.svg';
 
 const SignUp = () => {
     return (
         <div className="page-wrapper">
-            {/* Logo-ul în stânga */}
             <div className="logo">
                 <img src={LogoAlb} alt="Logo" />
             </div>
 
-            {/* Containerul cu conținutul de sign-up */}
             <div className="container">
+                {/* ✨ SCHIMBARE: Noul titlu */}
                 <div className="header">
-                    <div className="text">Sign Up</div>
+                    <div className="text">Înregistrează-te pe platforma noastră</div>
                 </div>
 
                 <div className="GoogleFB">
+                    {/* Buton Google */}
                     <div className="Google">
                         <img src={Google_Logo} alt="Google" />
-                        <a href="https://www.google.com/">Conectare prin Google</a>
+                        {/* ✨ SCHIMBARE: Noul text al butonului Google */}
+                        <a href="https://www.google.com/">Folosind Google</a>
                     </div>
+                    {/* Buton Facebook */}
                     <div className="Facebook">
                         <img src={Facebook_Logo} alt="Facebook" />
-                        <a href="https://www.facebook.com/">Conectare prin Facebook</a>
+                        {/* ✨ SCHIMBARE: Noul text al butonului Facebook */}
+                        <a href="https://www.facebook.com/">Folosind Facebook</a>
                     </div>
                 </div>
 
                 <div className="AltMod">Continuați în alt mod</div>
 
                 <div className="DejaCont">
-                    Aveți deja cont?
+                    Aveți deja un cont?
                     <a href="https://www.google.com/">Autentificare</a>
                 </div>
             </div>
