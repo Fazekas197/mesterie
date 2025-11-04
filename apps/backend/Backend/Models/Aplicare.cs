f9080547470b822733caeefe791700bad35dc8ab
Fazekas Cosmin <fazekascosmin1@gmail.com>
2025-11-04 23:26:53 +0200

    chore: format

--- apps/backend/Backend/Models/Aplicare.cs
+++ apps/backend/Backend/Models/Aplicare.cs
@@ -9,21 +9,24 @@ namespace Backend.Models;
 
 public class Aplicare
 {
-    [Key][ Required]
-    public int Id {get; set;}
-    [Required][ForeignKey("Oferta")]
-    public int Id_Oferta {get;set;}
-    [Required][ForeignKey("Meserias")]
-    public int Id_Meserias{get;set;}
+    [Key]
     [Required]
-    public string Mesaj {get; set;} = null!;
+    public int Id { get; set; }
+    [Required]
+    [ForeignKey("Oferta")]
+    public int Id_Oferta { get; set; }
+    [Required]
+    [ForeignKey("Meserias")]
+    public int Id_Meserias { get; set; }
+    [Required]
+    public string Mesaj { get; set; } = null!;
     [Required]
     public DateOnly Created_at { get; set; } = DateOnly.FromDateTime(DateTime.Now);
     [Required]
-    public string Status {get; set;}= null!;
+    public string Status { get; set; } = null!;
 
-    public Meserias Meserias{get; set;}=null!;
-    public Oferta Oferta{get;set;} = null!;
+    public Meserias Meserias { get; set; } = null!;
+    public Oferta Oferta { get; set; } = null!;
 
 
 }
--- apps/backend/Backend/Models/Review.cs
+++ apps/backend/Backend/Models/Review.cs
@@ -9,18 +9,19 @@ public class Review
     public int Id { get; set; }
     [Required]
     [ForeignKey("Utilizator")]
-    public int Id_User {get; set;} 
+    public int Id_User { get; set; }
     [ForeignKey("Meserias")]
-    public int Id_Meserias {get;set;}
-    [Required][Range(0, 5, ErrorMessage = "Value must be between 0 and 5.")]
-    public int? Rating {get;set;} = null!;
+    public int Id_Meserias { get; set; }
+    [Required]
+    [Range(0, 5, ErrorMessage = "Value must be between 0 and 5.")]
+    public int? Rating { get; set; } = null!;
 
-    public string Recenzie {get; set;} = null!;
+    public string Recenzie { get; set; } = null!;
 
-   public DateOnly Created_at { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+    public DateOnly Created_at { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
-    public Utilizator Utilizator {get; set;} = null!;
+    public Utilizator Utilizator { get; set; } = null!;
 
-    public Meserias Meserias{get;set;} = null!;
+    public Meserias Meserias { get; set; } = null!;
 
 }
