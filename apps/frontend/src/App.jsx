e635ae047162a57251741501fee56c715ed35e5f
anucacorne <anucacornea520@gmail.com>
2026-01-07 01:24:38 +0200

    Update App.jsx

--- apps/frontend/src/App.jsx
+++ apps/frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import Login from './Components/Login.jsx';
+import Login from './Components/SignUp/Login.jsx';
 import RoleSelection from './SelectareUser/RoleSelection.jsx';
 import Home from './Components/Home/Home.jsx';
 
@@ -47,40 +47,6 @@ function App() {
       )}
     </div>
   );
-=======
-import React, { useState } from "react";
-import "./App.css";
-import SignUp from "./Components/SignUp/SignUp";
-import Inregistrare from "./Components/SignUp/Inregistrare";
-import PagParola from "./Components/SignUp/PagParola";
-import RoleSelection from "./Components/SignUp/RoleSelection";
-import Login from "./Components/SignUp/Login"
-
-function App() {
-	const [paginaActiva, setPaginaActiva] = useState("signup");
-	const navigeazaLa = (pagina) => {
-		setPaginaActiva(pagina);
-		console.log("Navigare la pagina:", pagina);
-	};
-
-	return (
-		<div className="app-main-content">
-			{paginaActiva === "signup" && <SignUp onNavigare={navigeazaLa} />}
-			{paginaActiva === "login" && <Login onNavigare={navigeazaLa} />}
-
-			{paginaActiva === "inregistrare" && (
-				<Inregistrare onNavigare={navigeazaLa} />
-			)}
-
-			{paginaActiva === "parola" && (
-				<PagParola onNavigare={navigeazaLa} />
-			)}
-			{paginaActiva === "selectare-rol" && (
-				<RoleSelection onNavigare={navigeazaLa} />
-			)}
-		</div>
-	);
->>>>>>> main
 }
 
 export default App;
